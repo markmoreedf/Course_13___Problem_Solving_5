40d3ebe7ca9bc63ca68cc18a086533bbd46863f8
Mark Moreed <markmoreedf@gmail.com>
2026-09-20 19:50:31 +0300

    testing new data structures

--- Course_13___Problem_Solving_5.cpp
+++ Course_13___Problem_Solving_5.cpp
@@ -2,8 +2,10 @@
 #include "DblLinkedList.h"
 #include "MyQueue.h"
 #include "MyStack.h"
-#include <string>
-
+#include "DynamicArray.h"
+#include "MyQueueArr.h"
+#include "MyStackArr.h"
+#include "clsMyString.h"
 using namespace std;
 
 static void TestDblLinkedList()
@@ -206,10 +208,162 @@ static void TestMyStack()
     myStack.UpdateItemByIndex(2, 200);
     myStack.Print();
 }
+
+static void TestDynamicArray()
+{
+    DynamicArray<int> arr(5);
+    for (int i = 0; i < arr.Size(); ++i) {
+        arr.SetItem(i, i * 10);
+    }
+    std::cout << "Dynamic Array Elements: ";    arr.Print();
+    std::cout << "Size: " << arr.Size() << "\n";
+
+    arr.Reverse();
+    std::cout << "\n\nReversed Array Elements: ";
+    arr.Print();
+
+    arr.InsertAfter(2, 15);
+    std::cout << "\n\nInserted 15 after Index 2: ";
+    arr.Print();
+
+    arr.InsertAt(2, 25);
+    std::cout << "\n\nInserted 25 at Index 2: ";
+    arr.Print();
+
+    arr.InsertBefore(2, 35);
+    std::cout << "\n\nInserted 35 before Index 2: ";
+    arr.Print();
+
+    arr.InsertAtBeginning(45);
+    std::cout << "\n\nInserted 45 at Beginning: ";
+    arr.Print();
+
+    arr.InsertAtEnd(-5);
+    std::cout << "\n\nInserted -5 at End: ";
+    arr.Print();
+
+    arr.UpdateItemByIndex(2, 100);
+    std::cout << "\n\nUpdated Index 2 to 100: ";
+    arr.Print();
+
+    arr.DeleteItemByIndex(1);
+    std::cout << "\n\nDeleted Item at Index 1: ";
+    arr.Print();
+
+    arr.DeleteItemByValue(99);
+    std::cout << "\n\nDeleted Item with Value 99: ";
+    arr.Print();
+
+    arr.DeleteItemByValue(45);
+    std::cout << "\n\nDeleted Item with Value 45: ";
+    arr.Print();
+
+    arr.Resize(3);
+    std::cout << "\n\nResized Array Elements: ";
+    arr.Print();
+    std::cout << "New Size: " << arr.Size() << "\n";
+
+    arr.Resize(6);
+    std::cout << "\n\nResized Array Elements: ";
+    arr.Print();
+    std::cout << "New Size: " << arr.Size() << "\n";
+
+    arr.Clear();
+    arr.Print();
+    std::cout << "\n\nCleared Array. Is Empty? " << (arr.IsEmpty() ? "Yes" : "No") << "\n";
+
+}
+
+static void TestMyQueueArr()
+{
+    MyQueueArr<int> myQueueArr;
+    myQueueArr.Push(10);
+    myQueueArr.Push(20);
+    myQueueArr.Push(30);
+    myQueueArr.Push(40);
+    myQueueArr.Push(50);
+    myQueueArr.Push(60);
+    cout << "\nQueue Size: " << myQueueArr.Size() << "\n";
+    cout << "\nQueue Elements: ";     
+    myQueueArr.Print();
+    cout << "\nFront: " << myQueueArr.Front() << ", Back: " << myQueueArr.Back() << "\n";
+    myQueueArr.Pop();
+    myQueueArr.Pop(); 
+    myQueueArr.Pop();
+    cout << "\n\nQueue Elements after Popping 3 items: "; 
+    myQueueArr.Print();
+    cout << "\nFront: " << myQueueArr.Front() << ", Back: " << myQueueArr.Back() << "\n";
+    
+    cout << "\n\nGet item By index 0: " << myQueueArr.GetItem(0) << endl;
+    cout << "\n\nReverse the Queue: "; myQueueArr.Reverse();
+    myQueueArr.Print();
+    cout << "\n\nUpdate item at index 1 to 100: "; myQueueArr.UpdateItemByIndex(1, 100);
+    myQueueArr.Print();
+    cout << "\n\nInsert 5 at front: "; myQueueArr.InsertAtFront(5);
+    myQueueArr.Print();
+    cout << "\n\nInsert 70 at back: "; myQueueArr.InsertAtBack(70);
+    myQueueArr.Print();
+}
+
+static void TestMyStackArr()
+{
+    MyStackArr<int> myStackArr;
+    myStackArr.Push(10);
+    myStackArr.Push(20);
+    myStackArr.Push(30);
+    myStackArr.Push(40);
+    myStackArr.Push(50);
+    myStackArr.Push(60);
+    cout << "\nStack Size: " << myStackArr.Size() << "\n";
+    cout << "\nStack Elements: ";     
+    myStackArr.Print();
+    cout << "\nTop: " << myStackArr.Top() << ", Bottom: " << myStackArr.Bottom() << "\n";
+    myStackArr.Pop();
+    myStackArr.Pop(); 
+    myStackArr.Pop();
+    cout << "\n\nStack Elements after Popping 3 items: "; 
+    myStackArr.Print();
+    cout << "\nTop: " << myStackArr.Top() << ", Bottom: " << myStackArr.Bottom() << "\n";
+    
+    cout << "\n\nGet item By index 0: " << myStackArr.GetItem(0) << endl;
+    cout << "\n\nReverse the Stack: "; myStackArr.Reverse();
+    myStackArr.Print();
+    cout << "\n\nUpdate item at index 1 to 100: "; myStackArr.UpdateItemByIndex(1, 100);
+    myStackArr.Print();
+    cout << "\n\nInsert 5 at front: "; myStackArr.InsertAtFront(5);
+    myStackArr.Print();
+    cout << "\n\nInsert 70 at back: "; myStackArr.InsertAtBack(70);
+    myStackArr.Print();
+}
+
+static void TestMyString()
+{
+    clsMyString myString;
+    myString.SetValue("Hello");
+    cout << "Current Value: " << myString.GetValue() << endl;
+    myString.SetValue("World");
+    cout << "Current Value: " << myString.GetValue() << endl;
+    myString.SetValue("!");
+    cout << "Current Value: " << myString.GetValue() << endl;
+    myString.PrintHistory();
+    myString.Undo();
+    cout << "Current Value: " << myString.GetValue() << endl;
+    myString.Undo();
+    cout << "Current Value: " << myString.GetValue() << endl;
+    myString.Undo();
+    cout << "Current Value: " << myString.GetValue() << endl;
+    myString.Redo();
+    cout << "Current Value: " << myString.GetValue() << endl;
+    myString.Redo();
+    cout << "Current Value: " << myString.GetValue() << endl;
+    myString.Redo();
+    cout << "Current Value: " << myString.GetValue() << endl;
+}
+
+
 int main()
 {
-    TestMyStack();
-   
+    TestMyString();
     return 0;
 }
 
